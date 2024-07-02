791783ff12d6ad851d6697ea9d45a103d03908b7
arfanovi <arfanovi.gub1@gmail.com>
2024-07-02 21:57:25 +0600

    Set Program with Set Concept

---
+++ 27-Set.js
@@ -0,0 +1,22 @@
+
+
+//      ES6 Set
+/*
+    * Set is a collection of data
+    * Set is almost like array 
+    * But it does not contain any dependencies
+*/
+
+
+
+let mySet = new Set();
+mySet.add('Bangladesh');
+mySet.add('India');
+mySet.add('India');
+mySet.add('Nepal');
+mySet.add('Bhutan');
+mySet.add('China');
+mySet.add('Mal-dip');
+
+console.log(mySet);
+
---
+++ 28-SetPart2.js
@@ -0,0 +1,50 @@
+
+/*
+        ES6 Set 
+    * .clear()
+    * .delete(value)
+    * .has(value)
+    * .values()
+    * Set.size 
+*/
+
+
+
+
+let mySet = new Set();
+mySet.add('Bangladesh');
+mySet.add('India');
+mySet.add('India');
+mySet.add('Nepal');
+mySet.add('Bhutan');
+mySet.add('China');
+mySet.add('Mal-dip');
+
+
+// Clear Method 
+// mySet.clear();
+
+
+// Delete  Method 
+// mySet.delete('Nepal')
+
+
+// Size 
+// console.log(mySet.size)
+
+
+
+// Values 
+// console.log(mySet.values())
+
+
+// Has Method 
+if(mySet.has('India')){
+    console.log("Yes")
+} else{
+    console.log("No")
+}
+
+// console.log(mySet)
+
+
