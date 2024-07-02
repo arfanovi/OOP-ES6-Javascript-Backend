180fb1a552f15019fc3d4750a5ba6922d04e89f2
arfanovi <arfanovi.gub1@gmail.com>
2024-07-02 16:50:32 +0600

    Recape Functions and Returns

---
+++ 18-FunctionReturns.js
@@ -0,0 +1,19 @@
+
+function myFunction () {
+    return "Programmer";
+}
+
+console.log(myFunction())
+
+
+
+
+
+function x() {
+    return 20;
+}
+function y() {
+    return 21;
+}
+const xy = x() + y();
+console.log(xy)
