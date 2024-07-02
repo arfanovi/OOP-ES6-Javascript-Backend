da1f965cabb515dd4b1a83e1f12a0b15d1b3b32a
arfanovi <arfanovi.gub1@gmail.com>
2024-07-02 17:07:55 +0600

    Arrow Function

---
+++ 20-ArrowFunction.js
@@ -0,0 +1,20 @@
+
+
+/*
+     To write smaller function syntax
+     Arrow Functions make code more readable and structured
+     Arrow Functions are anonymous function 
+     Can declare without the function keyword
+
+     -------------------------------------------
+     Arrow functions cannot be used as the constructor
+
+*/
+
+
+
+
+var myArrowFun = (...x) => {
+    console.log(x)
+}
+myArrowFun(1,2,3,4,5)
