b86cd8a16e753dfb6f7c3501cb585677cd63d464
arfanovi <arfanovi.gub1@gmail.com>
2024-07-05 00:39:00 +0600

    Set

--- 0-OOP-Javascript/07-Getter-Setter.js
+++ 0-OOP-Javascript/07-Getter-Setter.js
@@ -15,10 +15,17 @@ class Phone {
     get phoneName () {
         return this.name;
     }
+    set phoneName(value) {
+        this.price = value;
+    }
 }
 
 const phone1 = new Phone("Vivo", "Y53");
 console.log(phone1);
 
+console.log(phone1.phoneName);
+console.log(phone1)
+
+phone1.phoneName = "Samsung";
 console.log(phone1.phoneName);
 console.log(phone1)
